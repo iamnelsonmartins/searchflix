509a832678a0d7120164e13054a18504c4a302f4
nelsonmartins <nelsonjmartins@gmail.com>
2019-10-07 23:43:13 +0100

    Initial commit

---
+++ code.js
@@ -0,0 +1,25 @@
+const results = document.querySelector("#results")
+const url = 'https://gist.githubusercontent.com/nelmarti/39f1ba049c8c9c1447db59d39415f9c6/raw/0262f2356ff7326037c82fd5f0cf0ea413c2575a/searchflix.json'
+
+const isSearched = searchTerm => item =>
+    item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+const onSearch = () => {
+    const searchTerm = document.querySelector("#searchBar").value
+    fetch(url)
+        .then(response => response.json())
+        .then(response => {
+            const list = response.filter(isSearched(searchTerm)).map(data => {
+                results.style.visibility = 'visible'
+                return `
+                    <div key=${data.id}>
+                        <span>
+                            <a href="${data.url}" target="_blank" rel="noopener noreferrer">${data.title}</a>
+                        </span>
+                    </div>
+            `
+            }).join('')
+            results.innerHTML = list
+        });
+}
+
