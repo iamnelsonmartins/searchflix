d85fc40b1471892589f75854e7bd970234dd326f
nelsonmartins <nelsonjmartins@gmail.com>
2019-10-08 21:36:15 +0100

    onSearch btn loading

--- code.js
+++ code.js
@@ -8,9 +8,9 @@ const onSearch = () => {
     const searchTerm = document.querySelector("#searchBar").value
     const buttonLoading = document.querySelector(".button")
     fetch(url)
-        .then(response => response.json())
+        .then(response => response.json(),
+            buttonLoading.classList.add("is-loading"))
         .then(response => {
-            buttonLoading.classList.add("is-loading")
             const list = response.filter(isSearched(searchTerm)).map(data => {
                 results.style.visibility = 'visible'
                 return `
@@ -19,10 +19,35 @@ const onSearch = () => {
                             <a href="${data.url}" target="_blank" rel="noopener noreferrer">${data.title}</a>
                         </span>
                     </div>
-            `
+                `
             }).join('')
-            results.innerHTML = list
+            buttonLoading.classList.remove("is-loading")
+            results.innerHTML = list;
         });
-    buttonLoading.classList.remove("is-loading")
 }
 
+
+/* begin begin Back to Top button  */
+const goTopBtn = document.querySelector('.backToTop');
+
+const trackScroll = () => {
+    let scrolled = window.pageYOffset;
+    let coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+        goTopBtn.style.visibility = "visible"
+    }
+    if (scrolled < coords) {
+        goTopBtn.style.visibility = "hidden"
+    }
+}
+
+const backToTop = () => {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 0);
+    }
+}
+
+window.addEventListener('scroll', trackScroll);
+goTopBtn.addEventListener('click', backToTop);
